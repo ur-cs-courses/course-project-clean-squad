3f0e4c56d5efb84b87a0dc1af33089825358162e
thelaurenxavier <115294128+thelaurenxavier@users.noreply.github.com>
2023-10-31 14:02:19 -0400

    Create simulation.hpp

---
+++ include/libhello/simulation.hpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Simulation {
+    private:
+        //vector<Robot> availableRobots;
+        //vector<Robot> unavailableRobots;
+        //vector<Room> roomList; 
+        //vector<Room> waitingQueue;
+        //vector<Room> cleaningRooms;
+        //vector<Room> completeRooms;
+    public:
+        //Simulation(vector<Robot> robots,vector<Room> rooms);
+        ~Simulation();
+        
+        void runSimulation();
+        void getInput();                                           //maybe can be in main
+
+        /*
+        Task createTask(); 
+        vector<Robot> getAvailableRobots();
+        vector<Robot> getUnavailableRobots();
+        vector<Room> getRoomList();
+        */
+
+};
