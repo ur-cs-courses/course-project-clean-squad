f95f38655c9212626e76cee9d3bcaff47d360485
michaelditrick <michaelmwalongo26@gmail.com>
2023-11-06 00:01:04 -0500

    Update Task.cpp

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,8 +1,24 @@
 #include "libclean/Task.hpp"
+// #include "libclean/Room.hpp"
+// #include "libclean/Robot.hpp"
 
 // Constructor
-Task::Task()
-    : numRobots(0), mopTime(0), vacuumTime(0), scrubTime(0), isCompleted(false) {}
+Task::Task() : numRobots(0), mopTime(0), vacuumTime(0), scrubTime(0), isCompleted(true) {}
+/*
+void Task::setNumRobots(int num) {
+    numRobots = num;
+}
+
+void Task::addWorkingRobot(Robot* robot) {
+    // Assuming Robot class has an interface to determine if it's already in the vector
+    // if (std::find(workingRobots.begin(), workingRobots.end(), robot) == workingRobots.end())
+    workingRobots.push_back(robot);
+}
+
+void Task::removeWorkingRobot(Robot* robot) {
+    // workingRobots.erase(std::remove(workingRobots.begin(), workingRobots.end(), robot), workingRobots.end());
+}
+*/
 
 // Getters
 int Task::getNumRobots() const {
@@ -45,3 +61,13 @@ void Task::setScrubTime(int scrubTime) {
 void Task::setIsCompleted(bool isCompleted) {
     this->isCompleted = isCompleted;
 }
+
+//others
+/*
+std::vector<Robot*> Task::getWorkingRobots() const {
+    return workingRobots;
+}
+Room* Task::getTaskLocation() const {
+    return taskLocation;
+}
+*/
