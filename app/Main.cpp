8656c96f10ab0fa76f0d564401315d8dacf1c65e
thelaurenxavier <lauren.xavier@richmond.edu>
2023-12-05 01:54:32 -0500

    made default simulation

--- app/Main.cpp
+++ app/Main.cpp
@@ -43,108 +43,166 @@ void writeToCSV(const std::vector<Robot>& robots, const std::vector<Room>& rooms
 
 int main() {
 
-    int numRobots, numRooms, robotType, robotSize, numTasks;
+    int numRobots, numRooms, robotType, robotSize, numTasks, choice;
     
     vector<Robot> robots;
     vector<Room> rooms;
     vector<Task> tasks;
     int count = 0;
 
-    // Ask the user for the number of robots
-    std::cout << "Enter the number of robots: ";
-    std::cin >> numRobots;
+    //Ask for custom or default
+    std::cout << "Would you like to use the default simulator or create a custom simulator (1: default, 2: custom): ";
+    std::cin >> choice;
+
+    if (choice == 2){
+        // Ask the user for the number of robots
+        std::cout << "Enter the number of robots: ";
+        std::cin >> numRobots;
 
 
-    for(int i = 0; i < numRobots; i++){
-        int typeInput, sizeInput;
-        RobotType typeEnum;
-        RobotSize sizeEnum;
-        std::cout << "\nInput for Robot " << count << std::endl;
-        std::cout << "Enter the type of robot (1: mopper, 2: vacuum, 3: scrubber): ";
-        std::cin >> typeInput;
+        for(int i = 0; i < numRobots; i++){
+            int typeInput, sizeInput;
+            RobotType typeEnum;
+            RobotSize sizeEnum;
+            std::cout << "\nInput for Robot " << count << std::endl;
+            std::cout << "Enter the type of robot (1: mopper, 2: vacuum, 3: scrubber): ";
+            std::cin >> typeInput;
 
-        switch (typeInput) {
-            case 1:
-                typeEnum = RobotType::mopper;
-                break;
-            case 2:
-                typeEnum = RobotType::vacuum;
-                break;
-            case 3:
-                typeEnum = RobotType::scrubber;
-                break;
-            default:
-                std::cerr << "Invalid robot type entered.\n";
-                continue;
-            }
+            switch (typeInput) {
+                case 1:
+                    typeEnum = RobotType::mopper;
+                    break;
+                case 2:
+                    typeEnum = RobotType::vacuum;
+                    break;
+                case 3:
+                    typeEnum = RobotType::scrubber;
+                    break;
+                default:
+                    std::cerr << "Invalid robot type entered.\n";
+                    continue;
+                }
 
-        std::cout << "Enter the size of robot (1: small, 2: medium, 3: large): ";
-        std::cin >> sizeInput;
+            std::cout << "Enter the size of robot (1: small, 2: medium, 3: large): ";
+            std::cin >> sizeInput;
 
-        switch (sizeInput) {
-            case 1:
-                sizeEnum = RobotSize::small;
-                break;
-            case 2:
-                sizeEnum = RobotSize::medium;
-                break;
-            case 3:
-                sizeEnum = RobotSize::large;
-                break;
-            default:
-                std::cerr << "Invalid robot size entered.\n";
-                continue;
-            }
+            switch (sizeInput) {
+                case 1:
+                    sizeEnum = RobotSize::small;
+                    break;
+                case 2:
+                    sizeEnum = RobotSize::medium;
+                    break;
+                case 3:
+                    sizeEnum = RobotSize::large;
+                    break;
+                default:
+                    std::cerr << "Invalid robot size entered.\n";
+                    continue;
+                }
 
-        Robot newRobot(typeEnum, sizeEnum);
-        robots.push_back(newRobot);
-        count++;
-    }
-
-    
-    // Ask the user for the number of rooms
-    std::cout << "\nEnter the number of rooms: ";
-    std::cin >> numRooms;
-    count = 0;
-
-    Room newRoom(RoomSize::home);
-    rooms.push_back(newRoom);
-
-    for(int i = 0; i < numRooms; i++){  
-
-        int sizeInput;
-        RoomSize sizeEnum;
-
-        std::cout << "Enter the type of room (1: small, 2: medium, 3: large): ";
-        std::cin >> sizeInput;
-
-        switch (sizeInput) {
-            case 1:
-                sizeEnum = RoomSize::small;
-                break;
-            case 2:
-                sizeEnum = RoomSize::medium;
-                break;
-            case 3:
-                sizeEnum = RoomSize::large;
-                break;
-            default:
-                std::cerr << "Invalid room type entered.\n";
-                continue;
-            }
-
-        Room newRoom(sizeEnum);
-
-        std::cout <<"Would you like to prevent the robots from cleaning this room? (1: yes)" ;
-        std::cin >> sizeInput;
-        if(sizeInput == 1){
-            newRoom.setClean(cleanStatus::doNotClean);
+            Robot newRobot(typeEnum, sizeEnum);
+            robots.push_back(newRobot);
+            count++;
         }
 
-        rooms.push_back(newRoom);
-        count++;
-    }
+        
+        // Ask the user for the number of rooms
+        std::cout << "\nEnter the number of rooms: ";
+        std::cin >> numRooms;
+        count = 0;
 
+        Room newRoom(RoomSize::home);
+        rooms.push_back(newRoom);
+
+        for(int i = 0; i < numRooms; i++){  
+
+            int sizeInput;
+            RoomSize sizeEnum;
+
+            std::cout << "Enter the type of room (1: small, 2: medium, 3: large): ";
+            std::cin >> sizeInput;
+
+            switch (sizeInput) {
+                case 1:
+                    sizeEnum = RoomSize::small;
+                    break;
+                case 2:
+                    sizeEnum = RoomSize::medium;
+                    break;
+                case 3:
+                    sizeEnum = RoomSize::large;
+                    break;
+                default:
+                    std::cerr << "Invalid room type entered.\n";
+                    continue;
+                }
+
+            Room newRoom(sizeEnum);
+
+            std::cout <<"Would you like to prevent the robots from cleaning this room? (1: yes)" ;
+            std::cin >> sizeInput;
+            if(sizeInput == 1){
+                newRoom.setClean(cleanStatus::doNotClean);
+            }
+
+            rooms.push_back(newRoom);
+            count++;
+        }
+
+    }
+    
+    else{
+        std::cout << "Here is the default simulator.";
+        numRobots = 13;
+
+        Robot newRobot1(RobotType::mopper, RobotSize::small);                                       //robot 1
+        robots.push_back(newRobot1);
+        Robot newRobot2(RobotType::mopper, RobotSize::medium);                                      //robot 2
+        robots.push_back(newRobot2);
+        Robot newRobot3(RobotType::mopper, RobotSize::large);                                       //robot 3
+        robots.push_back(newRobot3);
+        Robot newRobot4(RobotType::vacuum, RobotSize::small);                                        //robot 4
+        robots.push_back(newRobot4);
+        Robot newRobot5(RobotType::vacuum, RobotSize::medium);                                       //robot 5
+        robots.push_back(newRobot5);
+        Robot newRobot6(RobotType::vacuum, RobotSize::large);                                        //robot 6
+        robots.push_back(newRobot6);
+        Robot newRobot7(RobotType::scrubber, RobotSize::small);                                      //robot 7
+        robots.push_back(newRobot7);
+        Robot newRobot8(RobotType::scrubber, RobotSize::medium);                                     //robot 8
+        robots.push_back(newRobot8);
+        Robot newRobot9(RobotType::scrubber, RobotSize::large);                                      //robot 9
+        robots.push_back(newRobot9);
+        Robot newRobot0(RobotType::scrubber, RobotSize::medium);                                     //robot 10
+        robots.push_back(newRobot0);
+        Robot newRobot11(RobotType::mopper, RobotSize::large);                                        //robot 11
+        robots.push_back(newRobot11);
+        Robot newRobot12(RobotType::vacuum, RobotSize::small);                                        //robot 12
+        robots.push_back(newRobot12);
+        Robot newRobot13(RobotType::vacuum, RobotSize::small);                                        //robot 13
+        robots.push_back(newRobot13);
+       
+        numRooms = 6;
+
+        Room newRoom(RoomSize::home);
+        rooms.push_back(newRoom);
+
+        Room newRoom1(RoomSize::small);                                                            //room 1
+        rooms.push_back(newRoom1);
+        Room newRoom2(RoomSize::small);                                                            //room 2
+        rooms.push_back(newRoom2);
+        Room newRoom3(RoomSize::medium);                                                           //room 3 (do not clean)
+        newRoom3.setClean(cleanStatus::doNotClean);
+        rooms.push_back(newRoom3);
+        Room newRoom4(RoomSize::medium);                                                           //room 4
+        rooms.push_back(newRoom4);
+        Room newRoom5(RoomSize::large);                                                            //room 5
+        rooms.push_back(newRoom5);
+        Room newRoom6(RoomSize::large);                                                            //room 6
+        rooms.push_back(newRoom6);
+    }    
+    
     Simulation newSimulation(robots, rooms, tasks);
 
 // Write initial state to CSV
